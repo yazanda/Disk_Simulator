e56892bcbf52bb53fb11ee587bfdbecaaf5ac8ed
yazanda <yazandoifi@gmail.com>
2022-07-24 18:56:33 +0300

    updated

--- sim_disk.cpp
+++ sim_disk.cpp
@@ -12,29 +12,7 @@ using namespace std;
 
 #define DISK_SIM_FILE "DISK_SIM_FILE.txt"
 #define DISK_SIZE 80
-// ============================================================================
-//void decToBinary(int n, char &c)
-//{
-//    // array to store binary number
-//    int binaryNum[8];
-//
-//    // counter for binary array
-//    int i = 0;
-//    while (n > 0)
-//    {
-//        // storing remainder in binary array
-//        binaryNum[i] = n % 2;
-//        n = n / 2;
-//        i++;
-//    }
-//
-//    // printing binary array in reverse order
-//    for (int j = i - 1; j >= 0; j--)
-//    {
-//        if (binaryNum[j] == 1)
-//            c = c | 1u << j;
-//    }
-//}
+
 /***File System Class***/
 class FsFile {
     int file_size;
